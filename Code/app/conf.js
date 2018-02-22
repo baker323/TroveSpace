d4b5ecb01bb5a63792ff34b030d3e8185babf4ef
rorikata <rorikata@purdue.edu>
2018-02-22 09:30:46 -0500

    testing

--- Code/app/conf.js
+++ Code/app/conf.js
@@ -2,8 +2,8 @@ exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: [
-        //'register/register_test.js',
-        //'login/login_test.js',
+        'register/register_test.js',
+        'login/login_test.js',
         'troves/troves_test.js'
     ]
 }
--- Code/app/troves/troves_test.js
+++ Code/app/troves/troves_test.js
@@ -1,6 +1,77 @@
 describe('troves module', function() {
 
-    it('should successfully added to wishlist and remove from wishlist', function() {
+    it('should successfully added to wishlist', function() {
+        // login
+        browser.get('http://127.0.0.1:60788/#!/login');
+
+        element.all(by.model('email')).first().sendKeys('test@test.com');
+        element.all(by.model('password')).first().sendKeys('test18');
+
+        element(by.name('login')).click();
+
+        // add to wishlist
+        browser.sleep(2000);
+        element(by.name('viewTroves')).click();
+        browser.sleep(2000);
+
+        element(by.repeater('(key, value) in troves').row(0)).click();
+        browser.sleep(2000);
+
+        element(by.name('addButton')).click();
+        browser.sleep(2000);
+        element(by.name('addToWishlist')).click();
+        browser.sleep(2000);
+        expect(element(by.name('errorMessage')).getText()).toEqual("Item successfully added.");
+        element(by.name('confirm')).click();
+        browser.sleep(2000);
+
+        element(by.name('viewTroves')).click();
+        browser.sleep(2000);
+
+        element(by.repeater('(key, value) in troves').row(1)).click();
+        browser.sleep(2000);
+
+        element(by.name('addButton')).click();
+        browser.sleep(2000);
+        element(by.name('addToWishlist')).click();
+        browser.sleep(2000);
+        expect(element(by.name('errorMessage')).getText()).toEqual("Item successfully added.");
+        element(by.name('confirm')).click();
+        browser.sleep(2000);
+
+    });
+
+    it('should successfully remove all wishlist', function() {
+
+        browser.get('http://127.0.0.1:60788/#!/login');
+
+        element.all(by.model('email')).first().sendKeys('test@test.com');
+        element.all(by.model('password')).first().sendKeys('test18');
+
+        element(by.name('login')).click();
+
+        browser.sleep(2000);
+        element(by.name('myWishlist')).click();
+        browser.sleep(2000);
+        element(by.repeater('(key, value) in wishlist').row(0)).click();
+        browser.sleep(2000);
+        element(by.name('removeFromWishlist')).click();
+        browser.sleep(2000);
+
+
+        // remove
+        element(by.repeater('(key, value) in wishlist').row(0)).click();
+        browser.sleep(2000);
+        element(by.name('removeFromWishlist')).click();
+        browser.sleep(2000);
+        expect(element(by.name('errorMessage')).getText()).toEqual("There are currently no collectibles on your wishlist.");
+        element(by.name('confirm')).click();
+        browser.sleep(2000);
+
+    });
+
+
+    it('should deny for adding to collection', function() {
         // login
         browser.get('http://127.0.0.1:60788/#!/login');
 
@@ -13,24 +84,270 @@ describe('troves module', function() {
         browser.sleep(2000);
         element(by.name('viewTroves')).click();
         browser.sleep(2000);
-        element(by.name('collectibles')).click();
+
+        element(by.repeater('(key, value) in troves').row(0)).click();
         browser.sleep(2000);
+
         element(by.name('addButton')).click();
         browser.sleep(2000);
-        element(by.name('addToWishlist')).click();
+        element(by.name('error_addToFolder')).click();
+        browser.sleep(2000);
+        expect(element(by.name('errorMessage')).getText()).toEqual("There are no folders that match this collectible. Please create a folder.");
+        element(by.name('confirm')).click();
+        browser.sleep(2000);
+
+    });
+
+
+    it('should successfully make folder', function() {
+        // login
+        browser.get('http://127.0.0.1:60788/#!/login');
+
+        element.all(by.model('email')).first().sendKeys('test@test.com');
+        element.all(by.model('password')).first().sendKeys('test18');
+
+        element(by.name('login')).click();
+        // myCollection
+        browser.sleep(2000);
+        element(by.name('myCollection')).click();
+        browser.sleep(2000);
+        element(by.name('newFolder')).click();
+        browser.sleep(2000);
+
+        element.all(by.model('folderName')).first().sendKeys('test');
+        element(by.repeater('(key, value) in troves').row(0)).click();
+        browser.sleep(2000);
+        element(by.name('confirmFolder')).click();
+        browser.sleep(2000);
+        expect(element(by.name('errorMessage')).getText()).toEqual("There are currently no collectibles in this folder.");
+        element(by.name('confirm')).click();
+        browser.sleep(2000);
+
+
+    });
+
+    it('should rename the folder', function() {
+        // login
+        browser.get('http://127.0.0.1:60788/#!/login');
+
+        element.all(by.model('email')).first().sendKeys('test@test.com');
+        element.all(by.model('password')).first().sendKeys('test18');
+
+        element(by.name('login')).click();
+        browser.sleep(2000);
+        element(by.name('myCollection')).click();
+        browser.sleep(2000);
+        expect(element(by.name('errorMessage')).getText()).toEqual("There are currently no collectibles in this folder.");
+        element(by.name('confirm')).click();
+        browser.sleep(2000);
+        element(by.name('renameFolder')).click();
+        browser.sleep(2000);
+        element.all(by.model('newName')).first().sendKeys('TEST');
+        element(by.name('confirmNewName')).click();
+        browser.sleep(2000);
+        expect(element(by.name('errorMessage')).getText()).toEqual("There are currently no collectibles in this folder.");
+        element(by.name('confirm')).click();
+        browser.sleep(2000);
+
+    });
+
+    it('should add new collectives', function() {
+        // login
+        browser.get('http://127.0.0.1:60788/#!/login');
+
+        element.all(by.model('email')).first().sendKeys('test@test.com');
+        element.all(by.model('password')).first().sendKeys('test18');
+
+        element(by.name('login')).click();
+        browser.sleep(2000);
+        element(by.name('viewTroves')).click();
+        browser.sleep(2000);
+
+        element(by.repeater('(key, value) in troves').row(0)).click();
+        browser.sleep(2000);
+
+        element(by.name('addButton')).click();
+        browser.sleep(2000);
+        element(by.name('addToFolder')).click();
+        browser.sleep(2000);
+        element(by.name('confirmAddToCollection')).click();
         browser.sleep(2000);
         expect(element(by.name('errorMessage')).getText()).toEqual("Item successfully added.");
         element(by.name('confirm')).click();
         browser.sleep(2000);
 
-        // remove
-        element(by.name('myWishlist')).click();
+
+    });
+
+    it('should create another folder', function() {
+        // login
+        browser.get('http://127.0.0.1:60788/#!/login');
+
+        element.all(by.model('email')).first().sendKeys('test@test.com');
+        element.all(by.model('password')).first().sendKeys('test18');
+
+        element(by.name('login')).click();
+        // myCollection
         browser.sleep(2000);
-        element(by.name('removeFromWishlist')).click();
+        element(by.name('myCollection')).click();
         browser.sleep(2000);
-        expect(element(by.name('errorMessage')).getText()).toEqual("There are currently no collectibles on your wishlist.");
+        element(by.name('newFolder')).click();
+        browser.sleep(2000);
+
+        element.all(by.model('folderName')).first().sendKeys('hats');
+        element(by.repeater('(key, value) in troves').row(1)).click();
+        browser.sleep(2000);
+        element(by.name('confirmFolder')).click();
+        browser.sleep(2000);
+        //expect(element(by.name('errorMessage')).getText()).toEqual("There are currently no collectibles in this folder.");
+        //element(by.name('confirm')).click();
+        //browser.sleep(2000);
+    });
+
+    it('should deny creating the same folder', function() {
+        // login
+        browser.get('http://127.0.0.1:60788/#!/login');
+
+        element.all(by.model('email')).first().sendKeys('test@test.com');
+        element.all(by.model('password')).first().sendKeys('test18');
+
+        element(by.name('login')).click();
+        // myCollection
+        browser.sleep(2000);
+        element(by.name('myCollection')).click();
+        browser.sleep(2000);
+        element(by.name('newFolder')).click();
+        browser.sleep(2000);
+
+        element.all(by.model('folderName')).first().sendKeys('hats');
+        element(by.repeater('(key, value) in troves').row(1)).click();
+        browser.sleep(2000);
+        element(by.name('confirmFolder')).click();
+        browser.sleep(2000);
+        //expect(element(by.name('errorMessage')).getText()).toEqual("Folder with that name already exists.");
+        //element(by.name('confirm')).click();
+        //browser.sleep(2000);
+    });
+
+    it('should delete the folder', function() {
+        // login
+        browser.get('http://127.0.0.1:60788/#!/login');
+
+        element.all(by.model('email')).first().sendKeys('test@test.com');
+        element.all(by.model('password')).first().sendKeys('test18');
+
+        element(by.name('login')).click();
+        browser.sleep(2000);
+        element(by.name('myCollection')).click();
+        browser.sleep(2000);
+        element(by.repeater('(key, value) in collections').row(1)).click();
+        browser.sleep(2000);
+        expect(element(by.name('errorMessage')).getText()).toEqual("There are currently no collectibles in this folder.");
+        element(by.name('confirm')).click();
+        browser.sleep(2000);
+        element(by.name('deleteFolder')).click();
+        browser.sleep(2000);
+    });
+
+    it('should delete another folder', function() {
+        // login
+        browser.get('http://127.0.0.1:60788/#!/login');
+
+        element.all(by.model('email')).first().sendKeys('test@test.com');
+        element.all(by.model('password')).first().sendKeys('test18');
+
+        element(by.name('login')).click();
+        browser.sleep(2000);
+        element(by.name('myCollection')).click();
+        browser.sleep(2000);
+        element(by.repeater('(key, value) in collections').row(0)).click();
+        browser.sleep(2000);
+        element(by.name('deleteFolder')).click();
+        browser.sleep(2000);
+        expect(element(by.name('errorMessage')).getText()).toEqual("There are currently no collectibles in this folder.");
         element(by.name('confirm')).click();
         browser.sleep(2000);
     });
+
+/*
+    // testing create new collectibles
+    it('should create new collectible', function() {
+        // login
+        browser.get('http://127.0.0.1:60788/#!/login');
+
+        element.all(by.model('email')).first().sendKeys('test@test.com');
+        element.all(by.model('password')).first().sendKeys('test18');
+
+        element(by.name('login')).click();
+
+        // add to wishlist
+        browser.sleep(2000);
+        element(by.name('viewTroves')).click();
+        browser.sleep(2000);
+
+        element(by.repeater('(key, value) in troves').row(0)).click();
+        browser.sleep(2000);
+        element(by.name('newCollectible')).click();
+        browser.sleep(2000);
+        element.all(by.model('collectibleTitle')).first().sendKeys('test');
+        element.all(by.model('collectibleDescription')).first().sendKeys('test');
+        //element.all(by.repeater('(key, value) in trove')).first().sendKeys('test');
+        element.all(by.model('fieldValues[key]')).first().sendKeys('test');
+        browser.sleep(5000);
+        //element.all(by.model('email')).first().sendKeys('test');
+    });
+*/
+
+    it('should cancel the create Trove', function() {
+        // login
+        browser.get('http://127.0.0.1:60788/#!/login');
+
+        element.all(by.model('email')).first().sendKeys('test@test.com');
+        element.all(by.model('password')).first().sendKeys('test18');
+
+        element(by.name('login')).click();
+
+        // add to wishlist
+        browser.sleep(2000);
+        element(by.name('viewTroves')).click();
+        browser.sleep(2000);
+
+        element(by.repeater('(key, value) in troves').row(0)).click();
+        browser.sleep(2000);
+        element(by.name('newCollectible')).click();
+        browser.sleep(2000);
+        element(by.name('cancelCreateCollectible')).click();
+    });
+
+
+    // testing create new Troves
+    it('should create new Trove', function() {
+        // login
+        browser.get('http://127.0.0.1:60788/#!/login');
+
+        element.all(by.model('email')).first().sendKeys('test@test.com');
+        element.all(by.model('password')).first().sendKeys('test18');
+
+        element(by.name('login')).click();
+
+        // add to wishlist
+        browser.sleep(2000);
+        element(by.name('viewTroves')).click();
+        browser.sleep(2000);
+        element(by.name('addNewTrove')).click();
+        browser.sleep(2000);
+        //troveName
+        element.all(by.model('troveName')).first().sendKeys('TestTest');
+        element.all(by.model('troveDescription')).first().sendKeys('TestTest');
+        element(by.name('addField')).click();
+        browser.sleep(2000);
+        element.all(by.model('choice.name')).first().sendKeys('Test');
+        browser.sleep(2000);
+        element(by.name('removeChoice')).click();
+        browser.sleep(2000);
+        element(by.name('submitNewTrove')).click();
+        browser.sleep(2000);
+    });
+
 
 });
